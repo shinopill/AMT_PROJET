6db307386c6c021cc0f0b9e8c23a65243386a8c6
antoine.nourazar@heig-vd.ch <antoine.nourazar@heig-vd.ch>
2018-10-11 17:15:17 +0200

    model complete

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,5 +5,26 @@ public class User {
    private String name;
    private String email;
    private String password;
+   
+   public User(String name, String email, String password) {
+      
+      this.name  = name;
+      this.email = email;
+      this.password = password;
+   }
+   
+   public String getName() { return name; }
+   public String getId() { return email; }
 
+   public void setName(String newName) {
+      name = newName;
+   }
+   
+   public void setMail(String newMail) {
+      email = newMail;
+   }
+   
+   public void setPassword(String newPassword) {
+      password = newPassword;
+   }
 }
