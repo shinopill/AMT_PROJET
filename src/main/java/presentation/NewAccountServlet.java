aff5a947a7a2735c529778f004fb0e6e92c34b3f
antoine.nourazar@heig-vd.ch <antoine.nourazar@heig-vd.ch>
2018-10-11 18:33:07 +0200

    form registration

--- src/main/java/presentation/NewAccountServlet.java
+++ src/main/java/presentation/NewAccountServlet.java
@@ -1,13 +1,18 @@
 package presentation;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class NewAccountServlet extends HttpServlet  {
-   
+public class NewAccountServlet extends HttpServlet {
+
    @Override
-   public void doGet(HttpServletRequest request, HttpServletResponse response){
-      
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+      //make sure this is the path of the page you want/need to show
+      request.getRequestDispatcher("/WEB-INF/pages/registrationForm.jsp").forward(request, response);
    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,7 @@
 <html>
   <body>
     <h2>Hello World!</h2>
-    Click <a href="${pageContext.request.contextPath}/registration">here</a> to register
+    Click <a href="${pageContext.request.contextPath}/accountForm">here</a> to register
     Click <a href="${pageContext.request.contextPath}/signIn">here</a> to sign in
   </body>
 </html>
