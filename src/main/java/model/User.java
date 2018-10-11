18fbe4505dd227dbcfe5495a961446a9540e47e9
antoine.nourazar@heig-vd.ch <antoine.nourazar@heig-vd.ch>
2018-10-11 16:37:49 +0200

    premiers tests

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,8 +1,9 @@
 package model;
 
 public class User {
-    private String name;
-    private String email;
-    private String password;
+
+   private String name;
+   private String email;
+   private String password;
 
 }
--- src/main/java/presentation/NewAccountServlet.java
+++ src/main/java/presentation/NewAccountServlet.java
@@ -1,4 +1,13 @@
 package presentation;
 
-public class NewAccountServlet extends javax.servlet.http.HttpServlet  {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NewAccountServlet extends HttpServlet  {
+   
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response){
+      
+   }
 }
