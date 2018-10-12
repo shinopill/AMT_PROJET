73eb8e3b760beadca167c3f5f9ddfc107c8b408e
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-10-12 09:32:25 +0200

    add doGet

--- src/main/java/presentation/AdminServlet.java
+++ src/main/java/presentation/AdminServlet.java
@@ -1,4 +1,12 @@
 package presentation;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class AdminServlet extends javax.servlet.http.HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/pages/admin.jsp").forward(request, response);
+    }
 }
