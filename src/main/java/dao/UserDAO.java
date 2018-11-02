ce624008f67d6d33f203cfce1f8afb15b3f1273d
Florent <shinopill@gmail.com>
2018-11-02 09:45:14 +0100

    merge issue

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,9 +1,42 @@
 package dao;
 
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Stateless
 public class UserDAO implements UserDAOLocal {
+    // Add business logic below.
+    @Resource(lookup = "java:jboss/datasources/ExampleDS")
+    private DataSource dataSource;
+    private Connection connection;
+    public UserDAO() throws SQLException {
+       connection =  dataSource.getConnection();
+        System.out.print("ntiente");
+    }
 
-   
+    public String getUser(){
+        String sql = "SELECT ID FROM users";
+        PreparedStatement preparedStatement    = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+            if (!resultSet.next()) {
+
+                return resultSet.getString(1);
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
 }
--- src/main/java/dao/UserDAOLocal.java
+++ src/main/java/dao/UserDAOLocal.java
@@ -4,5 +4,6 @@ import javax.ejb.Local;
 
 @Local
 public interface UserDAOLocal {
-   
+
+
 }
